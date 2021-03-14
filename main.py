ee986f2c0ec1d938b25e786b244cfe90c6534eaa
johnbow <johnklein.dev@gmail.com>
2021-03-14 22:38:39 +0100

    stable build: - implemented plotting - fixed ^ character bug

--- main.py
+++ main.py
@@ -6,14 +6,24 @@ from eq.parsing import EqParser, ParserError
 
 class Viewport:
 
-    def __init__(self, parser, size=(600, 400), initial="0.1*x^2"):
+    def __init__(self, parser, size=(600, 400), initial="0.1*x^2", limits=(-10, 10, 0, 10)):
         self.parser = parser
         self.size = size
+        self.limits = limits
         self.current_function = None
         self.ax = None
         self.fig = None
         self.text_box = None
+        self.orig_key_press = None
+        self.suppress_submit = False
+        self.x_data, self.y_data = None, None
+        self.line = None
+        self.pixel_size = (800, 600)
+        self.num_points = 200
+        self.dpi = 100
         self.set_function(initial)
+        self.setup(initial)
+        self.setup_line_data()
 
     def set_function(self, expr):
         try:
@@ -21,35 +31,51 @@ class Viewport:
             assert len(f.var) == 1
             self.current_function = f
         except (ParserError, AssertionError):
-            self.text_box.set_val("invalid expression")
+            self.text_box.set_val("invalid")
 
-    def setup(self):
-        self.fig, self.ax = plt.subplots()
+    def setup_line_data(self):
+        self.x_data = np.linspace(*self.limits[:2], self.num_points)
+        self.y_data = self.current_function(self.x_data)
+        self.line, = self.ax.plot(self.x_data, self.y_data)
+
+    def compute_and_plot_line_data(self):
+        self.x_data = np.linspace(*self.limits[:2], self.num_points)
+        self.y_data = self.current_function(self.x_data)
+        self.line.set_data(self.x_data, self.y_data)
+
+    def setup(self, initial):
+        self.fig, self.ax = plt.subplots(figsize=(self.pixel_size[0]//self.dpi, self.pixel_size[1]//self.dpi),
+                                         num="Balls on functions - Simulation",
+                                         dpi=self.dpi)
         self.fig.subplots_adjust(bottom=0.2)
-        plt.xlim([-10, 10])
-        plt.ylim([0, 10])
+        plt.xlim(self.limits[:2])
+        plt.ylim(self.limits[2:])
         axbox = self.fig.add_axes([0.15, 0.05, 0.7, 0.06])
-        self.text_box = TextBox(axbox, "Plotten: ", initial=self.current_function.eqstr)
+        self.text_box = TextBox(axbox, "Plotten: ", initial=initial)
         self.text_box.on_submit(self.submit)
+        self.text_box.connect_event("key_release_event", self.custom_key_release)
 
-        # disable keyboard shortcuts
-        manager, canvas = self.fig.canvas.manager, self.fig.canvas
-        canvas.mpl_disconnect(manager.key_press_handler_id)
+    def custom_key_release(self, event):
+        # workaround of ^-key matplotlib bug:
+        if event.key is None:
+            self.suppress_submit = True
+            self.text_box.set_val(self.text_box.text + "^")
+            self.text_box.cursor_index += 1
+            self.text_box._rendercursor()
 
     def submit(self, expression):
+        if self.suppress_submit:
+            self.suppress_submit = False
+            return
         self.set_function(expression)
-        self.ax.relim()
-        self.ax.autoscale_view()
-        plt.draw()
+        self.compute_and_plot_line_data()
 
     def start_animation(self):
         plt.show()
 
-
 def launch():
     parser = EqParser()
     vp = Viewport(parser)
-    vp.setup()
     vp.start_animation()
 
 
